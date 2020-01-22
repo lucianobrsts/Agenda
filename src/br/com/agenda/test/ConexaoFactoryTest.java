9780f7083cd7c758c55d1f48579f449268ae699f
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-01-22 15:32:22 -0300

    Ignorado método de teste da conexão e modificado versão da página sobre

--- src/br/com/agenda/test/ConexaoFactoryTest.java
+++ src/br/com/agenda/test/ConexaoFactoryTest.java
@@ -3,6 +3,7 @@ package br.com.agenda.test;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.agenda.factory.ConexaoFactory;
@@ -11,6 +12,7 @@ public class ConexaoFactoryTest {
 
 	@SuppressWarnings("unused")
 	@Test
+	@Ignore
 	public void testaConexao() {
 		try {
 			Connection conexao = ConexaoFactory.conectar();
